12c48e8bb7fd3bb2c05fec0cd2c2a5f4eed0c99d
Bambusgamer <laucknerv2@gmail.com>
2023-02-21 11:28:09 +0100

    new bitexporter

--- src/util/bitfieldUtil.js
+++ src/util/bitfieldUtil.js
@@ -1,20 +1,20 @@
 module.exports = class BitfieldUtility {
     /**
      * Export a array of bitfield bits to a number
-     * @param {[Array, BigInt]} bits The array of bits
+     * @param {[[BigInt], [Number], BigInt, Number]} bits The array of bits
      * @return {Number} The number
      */
-    static exportBits(bits) {
-        if (!['number', 'array', 'bigint'].includes(typeof bits)) throw new Error('Bits must be a number, array, or bigint');
-        if (bits instanceof Number) return bits;
-        if (bits instanceof BigInt) return Number(bits);
+    static export(bits) {
+        if (typeof bits === 'bigint') return Number(bits);
+        if (typeof bits === 'number') return bits;
         if (bits instanceof Array) {
-            let bitfield = 0n;
+            if (bits.length === 0) return 0;
+            let out = 0n;
             for (const bit of bits) {
-                if (bit instanceof Number) bitfield |= BigInt(bit);
-                if (bit instanceof BigInt) bitfield |= bit;
+                if (typeof bit === 'bigint') out |= bit;
+                if (typeof bit === 'number') out |= BigInt(bit);
             }
-            return Number(bitfield);
+            return Number(out);
         }
     }
 };
